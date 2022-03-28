5738f7390a926f14a4edb65448ee93375bd2dba0
Kirill Buryakov <kirill_buryakov@outlook.com>
2022-03-28 22:13:26 +0500

    fourth sprint project: Project divided into files

---
+++ search-server/document.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "string_processing.h"
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+struct Document {
+    Document() = default;
+    Document(int id, double relevance, int rating)
+        : id(id)
+        , relevance(relevance)
+        , rating(rating) {
+    }
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+
+std::ostream& operator<<(std::ostream& out, const Document& document);
+
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+
+
+
